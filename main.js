93ade9a05e3bf98a4f76f94c58877aff94bc4fdb
NickkRomero <71767038+NickkRomero@users.noreply.github.com>
2023-09-26 12:40:57 +0200

    Cookie update

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ document.getElementById("acceptCookie").addEventListener("click", () => {
   let d = new Date();
   //Increment the current time by 1 minute (cookie will expire after 1 minute)
   d.setMinutes(2 + d.getMinutes());
-  //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+  //Create Cookie with name = myCookieName, value = thisIsMyCookie and expiry time = 1 minute
   document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
   //Hide the popup
-  popUp.classList.add("hide");
-  popUp.classList.remove("show");
+  popUp.style.visibility = "hidden"; // Verberg de popup
 });
+
 //Check if cookie is already present
 const checkCookie = () => {
   //Read the cookie and split on "="
