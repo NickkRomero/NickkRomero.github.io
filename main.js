b36dfb11cd29cab415086b7049c5dade5c1c6fe2
NickkRomero <71767038+NickkRomero@users.noreply.github.com>
2023-09-26 12:19:25 +0200

    Cookie update

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-function copyMail() {
-    navigator.clipboard.writeText('nk.nickromero@gmail.com');
-}
-
 let popUp = document.getElementById("cookiePopup");
-
+//When user clicks the accept button
 document.getElementById("acceptCookie").addEventListener("click", () => {
+  //Create date object
   let d = new Date();
+  //Increment the current time by 1 minute (cookie will expire after 1 minute)
   d.setMinutes(2 + d.getMinutes());
-  document.cookie = "myCookieName=thisIsMyCookie; expires=" + d + ";";
+  //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+  document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+  //Hide the popup
   popUp.classList.add("hide");
   popUp.classList.remove("show");
 });
-
+//Check if cookie is already present
 const checkCookie = () => {
-  let cookies = document.cookie.split(";").map(cookie => cookie.trim());
-  if (cookies.includes("myCookieName=thisIsMyCookie")) {
+  //Read the cookie and split on "="
+  let input = document.cookie.split("=");
+  //Check for our cookie
+  if (input[0] == "myCookieName") {
+    //Hide the popup
     popUp.classList.add("hide");
     popUp.classList.remove("show");
   } else {
+    //Show the popup
     popUp.classList.add("show");
     popUp.classList.remove("hide");
   }
 };
-
+//Check if cookie exists when page loads
 window.onload = () => {
-  checkCookie();
+  setTimeout(() => {
+    checkCookie();
+  }, 2000);
 };
