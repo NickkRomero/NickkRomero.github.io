4eeb2b33e811864bef4eaef66bd7efe3ac76c16c
NickkRomero <71767038+NickkRomero@users.noreply.github.com>
2023-09-26 12:17:14 +0200

    cookie banner test

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 function copyMail() {
     navigator.clipboard.writeText('nk.nickromero@gmail.com');
 }
+
+let popUp = document.getElementById("cookiePopup");
+
+document.getElementById("acceptCookie").addEventListener("click", () => {
+  let d = new Date();
+  d.setMinutes(2 + d.getMinutes());
+  document.cookie = "myCookieName=thisIsMyCookie; expires=" + d + ";";
+  popUp.classList.add("hide");
+  popUp.classList.remove("show");
+});
+
+const checkCookie = () => {
+  let cookies = document.cookie.split(";").map(cookie => cookie.trim());
+  if (cookies.includes("myCookieName=thisIsMyCookie")) {
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+  } else {
+    popUp.classList.add("show");
+    popUp.classList.remove("hide");
+  }
+};
+
+window.onload = () => {
+  checkCookie();
+};
